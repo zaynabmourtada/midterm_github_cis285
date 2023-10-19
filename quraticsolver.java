ce0472c291fcd34f447e2cf1d404fa4f52cb6cfd
Zaynab Mourtada <zaynabmo@umich.edu>
2023-10-19 17:23:11 -0400

    values changed

--- quraticsolver.java
+++ quraticsolver.java
@@ -3,9 +3,9 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 10;
-		int b = 30;
-		int c = 20;
+		int a = 12;
+		int b = 32;
+		int c = 200;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
 		else
